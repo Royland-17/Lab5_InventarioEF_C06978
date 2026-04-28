75df3bb09437edc99e5a77c8db40defcd74c8de3
Royland <royland170501@gmail.com>
2026-04-28 16:56:45 -0600

    Define el modelo Producto con validaciones y Data Annotations

---
+++ Lab5_InventarioEF_C06978/Models/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab5_InventarioEF_C06978.Models
+{
+    public class Producto
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        [Range(1, 9999999, ErrorMessage = "El precio debe estar entre 1 y 9,999,999")]
+        public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "La categoría es obligatoria")]
+        public string Categoria { get; set; }
+
+        public bool Activo { get; set; }
+
+        public DateTime FechaIngreso { get; set; } = DateTime.Now;
+    }
+}
