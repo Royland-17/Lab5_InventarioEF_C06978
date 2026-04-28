b206aaead73a47b8b97fa5112ea87dcca3fc7678
Royland <royland170501@gmail.com>
2026-04-28 15:33:30 -0600

    Agregar archivos de proyecto.

---
+++ Lab5_InventarioEF_C06978/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab5_InventarioEF_C06978.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
