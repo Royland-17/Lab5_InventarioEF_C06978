6024c4de6c85b2df7c16238d89fea69b274013fc
Royland <royland170501@gmail.com>
2026-04-28 16:54:53 -0600

    Define interfaz IProductoRepository con operaciones CRUD

---
+++ Lab5_InventarioEF_C06978/Data/IProductoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Lab5_InventarioEF_C06978.Models;
+
+namespace Lab5_InventarioEF_C06978.Data
+{
+    public interface IProductoRepository
+    {
+
+        IEnumerable<Producto> ObtenerTodos();
+        Producto ObtenerPorId(int id);
+        void Agregar(Producto p);
+        void Editar(Producto p);
+        void Eliminar(int id);
+    }
+}
