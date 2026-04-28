c5a00ca0ac700578fe238fe6152f686ad9179ed6
Royland <royland170501@gmail.com>
2026-04-28 16:52:46 -0600

    Configura AppDbContext y el mapeo de la entidad Producto

---
+++ Lab5_InventarioEF_C06978/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Lab5_InventarioEF_C06978.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab5_InventarioEF_C06978.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
