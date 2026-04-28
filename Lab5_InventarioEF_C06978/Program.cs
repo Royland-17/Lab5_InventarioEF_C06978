5e4dd16690dcb86269ad9b9fc0eab0b0875ae67d
Royland <royland170501@gmail.com>
2026-04-28 17:01:10 -0600

    Configura servicios de DbContext e Inyección de Dependencias en Program.cs

--- Lab5_InventarioEF_C06978/Program.cs
+++ Lab5_InventarioEF_C06978/Program.cs
@@ -1,4 +1,12 @@
+using Lab5_InventarioEF_C06978.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
